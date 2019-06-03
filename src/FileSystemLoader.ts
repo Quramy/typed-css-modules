e2a47697d29f3e8342774a705659103ae1ca2e14
Gerard Delmàs <gerard.delmas@wahanda.com>
2019-06-03 02:05:58 +0200

    use await in FileSystemLoader

--- src/FileSystemLoader.ts
+++ src/FileSystemLoader.ts
@@ -3,12 +3,16 @@
 import Core from 'css-modules-loader-core'
 import * as fs from 'fs'
 import * as path from 'path'
+import * as util from 'util'
 import { Plugin } from "postcss";
 
 
 type Dictionary<T> = {
   [key: string]: T | undefined;
 };
+
+const readFile = util.promisify(fs.readFile);
+
 
 export default class FileSystemLoader {
   private root: string;
@@ -18,55 +22,54 @@ export default class FileSystemLoader {
   public tokensByFile: Dictionary<Core.ExportTokens>;
 
   constructor( root: string, plugins?: Array<Plugin<any>> ) {
-    this.root = root
-    this.sources = {}
-    this.importNr = 0
-    this.core = new Core(plugins)
+    this.root = root;
+    this.sources = {};
+    this.importNr = 0;
+    this.core = new Core(plugins);
     this.tokensByFile = {};
   }
 
-  public fetch( _newPath: string, relativeTo: string, _trace?: string, initialContents?: string ): Promise<Core.ExportTokens> {
-    let newPath = _newPath.replace( /^["']|["']$/g, "" ),
-      trace = _trace || String.fromCharCode( this.importNr++ )
-    return new Promise( ( resolve, reject ) => {
-      let relativeDir = path.dirname( relativeTo ),
-        rootRelativePath = path.resolve( relativeDir, newPath ),
-        fileRelativePath = path.resolve( path.join( this.root, relativeDir ), newPath )
+  public async fetch(_newPath: string, relativeTo: string, _trace?: string, initialContents?: string): Promise<Core.ExportTokens> {
+    let newPath = _newPath.replace(/^["']|["']$/g, "");
+    let trace = _trace || String.fromCharCode(this.importNr++);
 
-      // if the path is not relative or absolute, try to resolve it in node_modules
-      if (newPath[0] !== '.' && newPath[0] !== '/') {
-        try {
-          fileRelativePath = require.resolve(newPath);
+    let relativeDir = path.dirname(relativeTo);
+    let rootRelativePath = path.resolve(relativeDir, newPath);
+    let fileRelativePath = path.resolve(path.join(this.root, relativeDir), newPath);
+
+    // if the path is not relative or absolute, try to resolve it in node_modules
+    if (newPath[0] !== '.' && newPath[0] !== '/') {
+      try {
+        fileRelativePath = require.resolve(newPath);
+      }
+      catch (e) {}
+    }
+
+    let source: string;
+
+    if (!initialContents) {
+      const tokens = this.tokensByFile[fileRelativePath]
+      if (tokens) {
+        return tokens;
+      }
+
+      try {
+        source = await readFile(fileRelativePath, "utf-8");
+      }
+      catch (error) {
+        if (relativeTo && relativeTo !== '/') {
+          return {};
         }
-        catch (e) {}
-      }
 
-      if(!initialContents) {
-        const tokens = this.tokensByFile[fileRelativePath]
-        if (tokens) { return resolve(tokens) }
-
-        fs.readFile( fileRelativePath, "utf-8", ( err, source ) => {
-          if ( err && relativeTo && relativeTo !== '/') {
-            resolve({});
-          }else if ( err && (!relativeTo || relativeTo === '/')) {
-            reject(err);
-          }else{
-            this.core.load( source, rootRelativePath, trace, this.fetch.bind( this ) )
-            .then( ( { injectableSource, exportTokens } ) => {
-              this.sources[trace] = injectableSource
-              this.tokensByFile[fileRelativePath] = exportTokens
-              resolve( exportTokens )
-            }, reject )
-          }
-        } )
-      }else{
-        this.core.load( initialContents, rootRelativePath, trace, this.fetch.bind(this) )
-        .then( ( { injectableSource, exportTokens } ) => {
-          this.sources[trace] = injectableSource
-          this.tokensByFile[fileRelativePath] = exportTokens
-          resolve( exportTokens )
-        }, reject )
+        throw error;
       }
-    } )
+    } else {
+      source = initialContents;
+    }
+
+    const { injectableSource, exportTokens } = await this.core.load(source, rootRelativePath, trace, this.fetch.bind(this));
+    this.sources[trace] = injectableSource;
+    this.tokensByFile[fileRelativePath] = exportTokens;
+    return exportTokens;
   }
 }
