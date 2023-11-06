95c5b6e30dc5506d0667bbf67da7793a331b9833
kana-takano@cybozu.co.jp <iricocco3@gmail.com>
2023-11-06 10:37:06 +0900

    add unlink watch

--- src/dts-creator.ts
+++ src/dts-creator.ts
@@ -42,7 +42,12 @@ export class DtsCreator {
     this.EOL = options.EOL || os.EOL;
   }
 
-  public async create(filePath: string, initialContents?: string, clearCache: boolean = false): Promise<DtsContent> {
+  public async create(
+    filePath: string,
+    initialContents?: string,
+    clearCache: boolean = false,
+    isDelete: boolean = false,
+  ): Promise<DtsContent> {
     let rInputPath: string;
     if (path.isAbsolute(filePath)) {
       rInputPath = path.relative(this.inputDirectory, filePath);
@@ -53,26 +58,26 @@ export class DtsCreator {
       this.loader.tokensByFile = {};
     }
 
-    const res = await this.loader.fetch(filePath, '/', undefined, initialContents);
-    if (res) {
-      const tokens = res;
-      const keys = Object.keys(tokens);
+    let keys: string[] = [];
+    if (!isDelete) {
+      const res = await this.loader.fetch(filePath, '/');
+      if (!res) throw res;
 
-      const content = new DtsContent({
-        dropExtension: this.dropExtension,
-        rootDir: this.rootDir,
-        searchDir: this.searchDir,
-        outDir: this.outDir,
-        rInputPath,
-        rawTokenList: keys,
-        namedExports: this.namedExports,
-        camelCase: this.camelCase,
-        EOL: this.EOL,
-      });
-
-      return content;
-    } else {
-      throw res;
+      keys = Object.keys(res);
     }
+
+    const content = new DtsContent({
+      dropExtension: this.dropExtension,
+      rootDir: this.rootDir,
+      searchDir: this.searchDir,
+      outDir: this.outDir,
+      rInputPath,
+      rawTokenList: keys,
+      namedExports: this.namedExports,
+      camelCase: this.camelCase,
+      EOL: this.EOL,
+    });
+
+    return content;
   }
 }
